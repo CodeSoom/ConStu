e8544d91e2f89091909503c1e89d2038e46b6f51
saseungmin <dbd02169@naver.com>
2020-11-18 22:34:12 +0900

    [Feature] Add webpack style loader
    - npm install style-loader, css-loader, sass-loader
    - Apply to index.jsx globally settings index.css

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import './index.css';
+
 import App from './App';
 
 import store from './reducers/store';
