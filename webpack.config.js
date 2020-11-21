2e552f826c0b42c7b8a67432f98d5cb981d12a57
saseungmin <dbd02169@naver.com>
2020-11-21 16:46:59 +0900

    [Fix] Fixed mac error and Improve webpack settings
    - npm install clean-webpack-plugin, file-loader, url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,59 @@
 const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const DEVELOPMENT_ENV = 'development';
+const PRODUCTION_ENV = 'production';
+
 const mode = process.env.NODE_ENV || DEVELOPMENT_ENV;
+
+const appBuild = path.resolve(__dirname, 'build');
+const appSrc = path.resolve(__dirname, 'src');
+const appIndex = path.resolve(__dirname, 'src', 'index.jsx');
+const appHtml = path.resolve(__dirname, 'public', 'index.html');
 
 module.exports = {
   mode,
-  entry: path.resolve(__dirname, 'src/index.jsx'),
+  entry: appIndex,
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.[hash].js',
+    path: appBuild,
+    filename: mode === PRODUCTION_ENV
+      ? 'static/js/[name].[contenthash:8].js'
+      : mode === DEVELOPMENT_ENV && 'static/js/bundle.js',
     publicPath: '/',
   },
   module: {
     rules: [
       {
-        test: /\.(scss|css)$/,
+        test: /\.css$/,
         use: [
           'style-loader',
           'css-loader',
-          'sass-loader',
         ],
+      },
+      {
+        test: /\.(jpe?g|PNG|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[hash:8].[ext]',
+          outputPath: 'static/media',
+          limit: 10000,
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        exclude: [/\.(js|jsx)$/, /\.html$/, /\.json$/],
+        options: {
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: 'babel-loader',
+        include: appSrc,
       },
     ],
   },
@@ -33,15 +61,17 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   devServer: {
+    overlay: true,
     historyApiFallback: true,
     hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: appHtml,
       templateParameters: {
         env: mode === DEVELOPMENT_ENV ? '(개발용)' : '',
       },
     }),
+    new CleanWebpackPlugin(),
   ],
 };
