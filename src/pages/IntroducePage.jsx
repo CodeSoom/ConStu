3c183427b57037b9e2334caf44a7adc053f682d8
saseungmin <dbd02169@naver.com>
2020-11-22 01:20:36 +0900

    Improve Study Introduce
    - Improve css style for study introduce page

--- src/pages/IntroducePage.jsx
+++ src/pages/IntroducePage.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 
 import { useParams } from 'react-router-dom';
 
-import styled from '@emotion/styled';
-
+import Responsive from '../styles/Responsive';
 import IntroduceContainer from '../containers/introduce/IntroduceContainer';
-
-const IntroducePageWrapper = styled.div``;
 
 const IntroducePage = ({ params }) => {
   const { id } = params || useParams();
 
   return (
-    <IntroducePageWrapper>
-      <h2>스터디 소개</h2>
+    <Responsive>
       <IntroduceContainer groupId={id} />
-    </IntroducePageWrapper>
+    </Responsive>
   );
 };
 export default IntroducePage;
--- src/pages/IntroducePage.test.jsx
+++ src/pages/IntroducePage.test.jsx
@@ -20,6 +20,7 @@ describe('IntroducePage', () => {
         title: '스터디를 소개합니다. 1',
         personnel: 7,
         contents: '우리는 이것저것 합니다.1',
+        participants: [],
         tags: [
           'JavaScript',
           'React',
