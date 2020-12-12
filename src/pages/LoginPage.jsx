227445109f94f9b1a0359bb0381d8746b99ca2f1
saseungmin <dbd02169@naver.com>
2020-12-13 00:21:58 +0900

    [Feature] Set title with react-helmet all pages

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { Helmet } from 'react-helmet';
+
 import LoginFormContainer from '../containers/auth/LoginFormContainer';
 
 const LoginPage = () => (
-  <LoginFormContainer />
+  <>
+    <Helmet>
+      <title>로그인</title>
+    </Helmet>
+    <LoginFormContainer />
+  </>
 );
 
 export default LoginPage;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { Helmet } from 'react-helmet';
+
 import RegisterFormContainer from '../containers/auth/RegisterFormContainer';
 
 const RegisterPage = () => (
-  <RegisterFormContainer />
+  <>
+    <Helmet>
+      <title>회원가입</title>
+    </Helmet>
+    <RegisterFormContainer />
+  </>
 );
 
 export default RegisterPage;
