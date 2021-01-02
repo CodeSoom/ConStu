065f32220de020bf17c683d1fe2ebf215b77da90
saseungmin <dbd02169@naver.com>
2021-01-02 21:37:54 +0900

    [Fix] coverage.yml

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
       statements: 100,
     },
   },
+  collectCoverage: true,
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
