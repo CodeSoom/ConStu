65529016bd1e40dc09aa23809e89c509a967dddf
saseungmin <dbd02169@naver.com>
2021-05-02 02:33:19 +0900

    [Update] Firebase api에 대한 테스트를 추가해라 #180
    - 누락된 Firebase API 테스트를 추가

--- src/__mocks__/firebase/app.js
+++ src/__mocks__/firebase/app.js
@@ -1,14 +1,27 @@
 const firebase = jest.genMockFromModule('firebase/app');
 
+const mockOrderBy = {
+  orderBy: jest.fn().mockImplementation(() => ({
+    get: jest.fn().mockReturnValue({
+      docs: [],
+    }),
+  })),
+};
+
 firebase.firestore = jest.fn().mockImplementation(() => ({
   collection: jest.fn().mockImplementation(() => ({
     doc: jest.fn().mockImplementation(() => ({
       update: jest.fn(),
       delete: jest.fn(),
+      get: jest.fn(),
+      set: jest.fn(),
     })),
+    add: jest.fn().mockReturnValue({ id: 'id' }),
+    where: jest.fn().mockReturnValue(mockOrderBy),
+    ...mockOrderBy,
   })),
 }));
 
-firebase.auth = jest.fn().mockImplementation(() => ({}));
+firebase.auth = jest.fn().mockReturnValue({});
 
 export default firebase;
