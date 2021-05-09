d1345b0dc26b6832a1b52095b130164d15ce7de5
saseungmin <dbd02169@naver.com>
2021-05-09 23:43:13 +0900

    [Feature] theme toggle 버튼 만들어라 #171
    - Add svg to be used for theme toggle button
    - npm install react-toogle dependencies

---
+++ src/styles/ThemeToggle.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+
+import Toggle from 'react-toggle';
+
+import SunIcon from '../assets/icons/sun.svg';
+import MoonIcon from '../assets/icons/moon.svg';
+
+import mq from './responsive';
+
+import 'react-toggle/style.css';
+
+const ToggleWrapper = styled(Toggle)`
+  &.react-toggle .react-toggle-track {
+    background-color: #ffa000;
+  }
+
+  &.react-toggle .react-toggle-track {
+  ${mq({
+    width: ['55px', '60px'],
+    height: ['27px', '30px'],
+  })};
+  }
+
+  &.react-toggle .react-toggle-thumb {
+  ${mq({
+    width: ['27px', '30px'],
+    height: ['27px', '30px'],
+  })};
+
+    top: 0px;
+    left: 0px;
+    border: 2px solid #ffa000;
+  }
+
+  &.react-toggle--checked .react-toggle-thumb {
+  ${mq({
+    left: ['28px', '31px'],
+  })};
+
+    border: 2px solid #4D4D4D;
+  }
+
+  &.react-toggle--checked .react-toggle-track {
+    background-color: #4D4D4D;
+  }
+
+  &.react-toggle .react-toggle-track-check {
+  ${mq({
+    width: ['26px', '28px'],
+    height: ['26px', '28px'],
+    left: ['3px', '4px'],
+  })};
+  }
+
+  &.react-toggle .react-toggle-track-x {
+    ${mq({
+    width: ['23px', '24px'],
+    height: ['23px', '24px'],
+    right: ['3px', '4px'],
+  })};
+  }
+
+  &.react-toggle:hover:not(.react-toggle--disabled) .react-toggle-track {
+    background-color: #f57c00;
+  }
+
+  &.react-toggle--checked:hover:not(.react-toggle--disabled) .react-toggle-track {
+    background-color: #000000;
+  }
+
+  &.react-toggle--focus .react-toggle-thumb {
+    box-shadow: none;
+  }
+
+  &.react-toggle:active:not(.react-toggle--disabled) .react-toggle-thumb {
+    box-shadow: none;
+  }
+`;
+
+const ThemeToggle = ({ onChange, theme }) => (
+  <ToggleWrapper
+    defaultChecked={theme}
+    title={theme ? 'dark' : 'light'}
+    aria-label="No label tag"
+    onChange={onChange}
+    icons={{
+      checked: <MoonIcon />,
+      unchecked: <SunIcon />,
+    }}
+    data-testid="theme-toggle"
+  />
+);
+export default ThemeToggle;
