6369e83473907f3ec0f86e46fd642cf2952950d2
saseungmin <dbd02169@naver.com>
2021-07-06 21:15:11 +0900

    [Remove] 불필요한 주석 삭제

--- src/reducers/groupSlice.test.js
+++ src/reducers/groupSlice.test.js
@@ -156,42 +156,6 @@ describe('reducer', () => {
     });
   });
 
-  // describe('changeApplyFields', () => {
-  //   it('changes a field of study participation application form', () => {
-  //     const initialState = {
-  //       applyFields: {
-  //         reason: '',
-  //         wantToGet: '',
-  //       },
-  //     };
-
-  //     const state = reducer(
-  //       initialState,
-  //       changeApplyFields({ name: 'reason', value: '참여합니다.' }),
-  //     );
-
-  //     expect(state.applyFields.reason).toBe('참여합니다.');
-  //   });
-  // });
-
-  // describe('clearApplyFields', () => {
-  //   const initialState = {
-  //     applyFields: {
-  //       reason: '타이틀',
-  //       wantToGet: '내용',
-  //     },
-  //   };
-
-  //   it('clears fields of application', () => {
-  //     const state = reducer(initialState, clearApplyFields());
-
-  //     const { applyFields: { reason, wantToGet } } = state;
-
-  //     expect(reason).toBe('');
-  //     expect(wantToGet).toBe('');
-  //   });
-  // });
-
   describe('setOriginalArticle', () => {
     const initialState = {
       originalArticleId: null,
