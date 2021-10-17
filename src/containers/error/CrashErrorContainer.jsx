214c4a50d4f7a15838f9a9684157fecfa9ef0433
Harang <dbd02169@naver.com>
2021-10-17 21:05:22 +0900

    [Feature] Crash Error Page에 이미지를 적용하라 (#238)

--- src/containers/error/CrashErrorContainer.jsx
+++ src/containers/error/CrashErrorContainer.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 import { useHistory } from 'react-router-dom';
 
+import styled from '@emotion/styled';
+
+import mq from '../../styles/responsive';
+
+import ComputerSvg from '../../assets/icons/computer.svg';
 import ErrorScreenTemplate from '../../components/error/ErrorScreenTemplate';
+
+const ComputerImage = styled(ComputerSvg)`
+  ${mq({
+    width: ['500px', '800px', '900px'],
+  })};
+
+  height: auto;
+`;
 
 const CrashErrorContainer = ({ onResolve }) => {
   const history = useHistory();
@@ -17,7 +30,11 @@ const CrashErrorContainer = ({ onResolve }) => {
       message="이런.. 오류가 발생했어요!"
       buttonText="홈으로"
       onClick={onClick}
-    />
+    >
+      <ComputerImage
+        data-testid="computer-image"
+      />
+    </ErrorScreenTemplate>
   );
 };
 
